ee97ca409544e72d5c93cfc23607926057c93b9e
bjbfr <benjamin.bouchard.fr@gmail.com>
2023-10-26 15:05:34 +0200

    add load handler

--- code/pilotage.ts
+++ code/pilotage.ts
@@ -1,16 +1,79 @@
 namespace Pilotage {
 
-    export function init_handler() {
-        Ui.alert_user("Init handler", "hello");
-    }
+  export function init_handler() {
+    Ui.alert_user("Init handler", "Hello, World!");
+  }
+  /**
+   * 
+   */
+  export function load_handler() {
+    // sheet where to store transactions
+    const sheet = Spreadsheet.get_sheet_by_property("transactionsSheetName");
+    const pivotSheet = Spreadsheet.get_sheet_by_property("pivotTableSheetName");
 
-    export function onOpen() {
-        Ui.add_menu(
-          'SASU BB-IMMO',
-          [
-            {item:'Init',handler:`Pilotage.${Pilotage.init_handler.name}`}
-          ]
-        );
+    if (sheet) {
+      //folder where to find src folders
+      const dataSrcFolder = Properties.getProperty("dataSrcFolder");
+      // list src folders
+      const folders = Fs.list_dirs(dataSrcFolder);
+      // foreach src folder
+      let res = [] as Array<{ src: string, n: number }>
+      for (const { id: dirId, name } of folders) {
+        const files = Fs.list_files(dirId);
+        for (const fileDesc of files) {
+          const transactions = get_transactions(fileDesc, name);
+          if (sheet && transactions) {
+            // synchronize sheet with identifiers: ["Date", "Montant", "Counterpart"]
+            const n = Spreadsheet.synchronize(sheet, transactions, ["Date", "Montant", "Counterpart"]);
+            if (n > 0)
+              res.push({ src: `${name}/${fileDesc.name}`, n: n });
+          }
+        }// for
+      }// for
+
+      //set columns auto-size and filter and banding
+      if (res.length > 0) {
+        const limits = Spreadsheet.get_limits(sheet);
+        if (limits) {
+          const range = Spreadsheet.get_tabular_range(sheet, limits);
+          // auto-size
+          Spreadsheet.auto_size_columns(sheet, limits);
+          //filter
+          Spreadsheet.remove_filter(sheet);
+          Sheetrange.set_filter(range);
+          //banding
+          Sheetrange.set_banding(range, SpreadsheetApp.BandingTheme.BLUE);
+          // add pivot table
+          // const { firstRow, lastColumn } = limits;
+          // const anchor = sheet.getRange(firstRow, lastColumn + 2);
+          // Spreadsheet.remove_pivot_table(sheet);
+          if (pivotSheet)
+            Spreadsheet.add_pivot_table(pivotSheet, range, ["Counterpart"], ["AccountingYear", "Date"], [["Montant", SpreadsheetApp.PivotTableSummarizeFunction.SUM]])
+        }
+
+        const msg = "Transactions added:\n" + res.map(({ src, n }) => `- ${src}: ${n}`).join('\n')
+        Ui.alert_user("Load transactions", msg);
+      }
+      else {
+        Ui.alert_user("Load transactions", "No transactions added.");
+      }
+    } else {
+      const msg = `Cannot find sheet to add transactions.`;
+      console.log(msg);
+      Ui.alert_user("Load transactions", msg);
     }
+  }
+  /**
+   * 
+   */
+  export function onOpen() {
+    Ui.add_menu(
+      'SASU BB-IMMO',
+      [
+        { item: 'Init', handler: `Pilotage.${Pilotage.init_handler.name}` },
+        { item: 'Load', handler: `Pilotage.${Pilotage.load_handler.name}` }
+      ]
+    );
+  }
 
 }
--- code/tests/pilotage.test.ts
+++ code/tests/pilotage.test.ts
@@ -1,11 +1,29 @@
 /// <reference path="../pilotage.ts"/>
 
-function pilotage_test_(){
+function pilotage_test_synchronize() {
 
+    const sheet = SpreadsheetApp.openById('1EV9xKQSOR9Lf_oeMyM9zKTG2ffiXKmZu6Fu0NfIf2zY').getSheetByName('Transactions');
+    const shine_data = Pilotage.get_transactions({ id: "1MV-SUmXsFnL8smh1l63zWy328bDlHXI1", name: "BQ_07-2023_02-10-2023.csv", type: Fs.MineType.TEXT }, "shine");
+    if (sheet && shine_data)
+        Spreadsheet.synchronize(sheet, shine_data, ["Date", "Montant", "Counterpart"])
+}
 
+function pilotage_test_qonto1() {
+    const res = Pilotage.get_transactions({ id: "1yoIOIv6jEzycpkc7Vwk5OjM1RquOqtJr", name: "2023-10-12_19-58-30_bb_immo_qonto_extended.csv", type: Fs.MineType.TEXT }, "qonto1")
+    return res
+}
+
+function pilotage_test_qonto2() {
+    const res = Pilotage.get_transactions({ id: "1J5B7jgrQEDnJ2UfdHIra6M3gAsQrBfyz", name: "2023-10-10_20-06-10_bb_immo.csv", type: Fs.MineType.TEXT }, "qonto2");
+    return res
+}
+
+function pilotage_test_shine() {
+    const res = Pilotage.get_transactions({ id: "1MV-SUmXsFnL8smh1l63zWy328bDlHXI1", name: "BQ_07-2023_02-10-2023.csv", type: Fs.MineType.TEXT }, "shine");
+    return res
 }
 
 // test_all
-function pilotage_test_all(){
+function pilotage_test_all() {
     return Unittest.run_all([])
 }
